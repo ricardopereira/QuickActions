0ee20df49300bd789ecb36b1e18f9b5c941d13f7
Ricardo Pereira <ricardopereira.eu@gmail.com>
2019-05-14 23:58:32 +0100

    Update Example

--- Example/QuickActionsExample/AppDelegate.swift
+++ Example/QuickActionsExample/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var quickActions: QuickActions<AppShortcut>?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = MainViewController()
